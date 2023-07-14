960c8e10cb8e38d00434d4fa9fd764c16d0a6822
Mayuresh <kmayuresh7@gmail.com>
2023-07-14 20:04:46 +0530

    added notes

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const head1 = React.createElement(
-    "h1",
-    {id:"title"},
-    "Hi mayuresh Welcome to Namaste React by akshay"
-  );
-  const head2 = React.createElement(
-    "h2",
-    {id:"title"},
-    "this is cool stuff"
-  );
-  
-  const container = React.createElement("div",{
-    id:'container'
-  },[head1,head2])
-  
-  root.render(container);
+  "h1",
+  { id: "title" },
+  "Hi mayuresh Welcome to Namaste React by akshay"
+);
+const head2 = React.createElement("h2", { id: "title" }, "this is cool stuff");
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [head1, head2]
+);
+
+root.render(container);
