bfcf605d0af2da54fedf863cf12735a641bc1a48
Mayuresh <kmayuresh7@gmail.com>
2023-07-18 11:37:13 +0530

    functional component and functional composition

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-
 /*
-HMR - hot modeule reloading - responsible for reaload page when updated or make changes.
+HMR - hot modeule replacing - responsible for reaload page when updated or make changes.
 file watcher algorithm - written in C++ 
 Bundling the code 
 Minify
@@ -11,28 +10,64 @@ image optimization also done by parcel and caching while development
 provides the https for dev build ==> npx parcel index.html --https
 "main": "App.js", removed from package.json bcoz for parcel 
 
+====================
+// Functional component = New way to write 
+// class based component = old way to write 
+
 */
-
-
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// React Element
 const head1 = React.createElement(
   "h1",
-  { id: "title" },
+  { id: "title", key: "1" },
   "Hi mayuresh Welcome to Namaste React by akshay"
 );
-const head2 = React.createElement("h2", { id: "title" }, "use of parcel to bundel and create the react app from scratch");
+// console.log(head1); // returns the js object this means any object is a react element
+
+const head2 = React.createElement(
+  "h2",
+  { id: "title", key: "2" },
+  "Use of parcel to bundel and Parcel is a Bundler and create the react app from scratch"
+);
+const head3 = (
+  <h3 id="titile" key="3">
+    This is also react Element
+  </h3>
+);
 
 const container = React.createElement(
   "div",
   {
     id: "container",
   },
-  [head1, head2]
+  [head1, head2, head3]
 );
 
-root.render(container);
+// React functional component
+const HeaderComponent = () => {
+  // return <h1>First functional component</h1>;
+  return (
+    // react fragment
+    <>
+      {container}
+      <h1>Functional component</h1>
+      <h2>h2 element</h2>
+    </>
+  );
+};
+
+const HeaderComponent2 = () => (
+  // react fragment
+  <>
+    <h1>component</h1>
+    <h2>component</h2>
+  </>
+);
+
+// root.render(container); // way to render the react element
+root.render(<HeaderComponent />); // way to render the react component
---
+++ notesandimp.md
@@ -0,0 +1,3 @@
+https://browserslist.dev/?q=bGFzdCAxNSB2ZXJzaW9ucw%3D%3D
+
+https://www.npmjs.com/package/babel-plugin-transform-remove-console
