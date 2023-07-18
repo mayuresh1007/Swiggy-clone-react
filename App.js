198b06c93cdab9fae6abeb54fac4324ed9aad4aa
Mayuresh <kmayuresh7@gmail.com>
2023-07-18 22:46:52 +0530

    write the video 5 code making config Driven UI

--- App.js
+++ App.js
@@ -19,9 +19,11 @@ provides the https for dev build ==> npx parcel index.html --https
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const notes = ReactDOM.createRoot(document.getElementById("notes"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // React Element
+
 const head1 = React.createElement(
   "h1",
   { id: "title", key: "1" },
@@ -49,11 +51,19 @@ const container = React.createElement(
 );
 
 // React functional component
+// const Title = () => {
+//   return <h1>Foodora</h1>;
+// };
+const Title = () => <h1>Foodora</h1>;
+
 const HeaderComponent = () => {
   // return <h1>First functional component</h1>;
   return (
     // react fragment
     <>
+      {/* {Title()} // method one to call the functional component */}
+      {/* <Title /> // method two and good practice to call the functional component */}
+      <Title />
       {container}
       <h1>Functional component</h1>
       <h2>h2 element</h2>
@@ -68,6 +78,113 @@ const HeaderComponent2 = () => (
     <h2>component</h2>
   </>
 );
-
 // root.render(container); // way to render the react element
-root.render(<HeaderComponent />); // way to render the react component
+// notes.render(<HeaderComponent />); // way to render the react component
+
+/* <React.Fragment></React.Fragment> React fragment is a component which is exported by react
+<React.Fragment></React.Fragment> ==>> <></>
+
+*/
+
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
+    </React.Fragment>
+  );
+};
+const Logo = () => (
+  <>
+    <a href="/">
+      <img
+        src="https://cdn.dribbble.com/users/333713/screenshots/16263237/media/e13f7d89feec1544447e4e76f09836a5.png?compress=1&resize=400x300&vertical=center"
+        alt="Logo"
+        className="logo"
+      />
+    </a>
+    {/* <h1 className="title">FlashFood</h1> */}
+  </>
+);
+const AppHeader = () => (
+  <div className="header">
+    <Logo />
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const AppBody = () => {
+  return (
+    <div className="restro-list">
+      <RestraurantCard />
+      <RestraurantCard />
+      <RestraurantCard />
+      <RestraurantCard />
+      <RestraurantCard />
+    </div>
+  );
+};
+/**
+ Restraurants card
+  - Menu
+  - Price
+  - category
+  - tag
+ */
+
+const Burger = [
+  {
+    name: "Burger King",
+    price: "100",
+    rating: "4.2 ",
+    image:
+      "https://e7.pngegg.com/pngimages/908/351/png-clipart-mattar-paneer-indian-cuisine-paneer-tikka-masala-vegetarian-cuisine-paneer-soup-food-thumbnail.png",
+    cuisine: ["Veg-Burger", "Nonveg", "egg -Burger"],
+  },
+  {
+    name: "Paneer",
+    price: "150",
+    rating: "4.4 ",
+    image:
+      "https://e7.pngegg.com/pngimages/908/351/png-clipart-mattar-paneer-indian-cuisine-paneer-tikka-masala-vegetarian-cuisine-paneer-soup-food-thumbnail.png",
+    cuisine: ["Veg-kadhai", "veg-Bhaji", "palak panner"],
+  },
+];
+// console.log(Burger[0].cuisine )
+// Burger.map(cuisine=>{console.log(cuisine)})
+const RestraurantCard = () => (
+  <div className="card">
+    <img src={Burger.image} alt="panner kadhai" />
+    <h3>{"Manu -" + Burger.name}</h3>
+    <h4>
+      {"Price -" + Burger.price} <span>&#8377;</span>{" "}
+    </h4>
+    <h4>{Burger.rating} Stars</h4>
+    <p>{Burger.map(cuisine=>)} </p>
+  </div>
+);
+// const RestraurantCard = () => (
+//   <div className="card">
+//     <img
+//       src="https://e7.pngegg.com/pngimages/908/351/png-clipart-mattar-paneer-indian-cuisine-paneer-tikka-masala-vegetarian-cuisine-paneer-soup-food-thumbnail.png"
+//       alt="panner kadhai"
+//     />
+//     <h3>Manu - Panner cadhai</h3>
+//     <h4>
+//       Price - 100 <span>&#8377;</span>{" "}
+//     </h4>
+//     <h4>Rating - 4.3 stars</h4>
+//   </div>
+// );
+const AppFooter = () => {
+  return <h3>Footer</h3>;
+};
+root.render(<AppLayout />); // way to render the react component
